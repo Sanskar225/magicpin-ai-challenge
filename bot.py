5a65e9a8453e4de0d70ecd68653a3f5c035387b3
Sanskar Sinha <sanskarsinha225@gmail.com>
2026-09-01 18:36:23 +0530

    Set local port to 8000 for local testing without socket conflict

--- bot.py
+++ bot.py
@@ -363,4 +363,4 @@ async def reply(body: ReplyRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("bot:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("bot:app", host="127.0.0.1", port=8000, reload=True)
